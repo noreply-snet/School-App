84235b27cd78b10ebcf5d9bdf11ba6f5a8004b4e
noreply <noreply.snet@gmail.com>
2024-06-16 13:56:27 +0530

    test1 done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { P404Component } from './pages/components/p404/p404.component';
 
 const routes: Routes = [
   {
-    path:'',
-    component: HomeComponent
-}
+    path: '', 
+    children: [
+      {
+        path: '',
+        component: HomeComponent,
+      }
+    ]
+  },
+  {
+    path: 'a',
+    component: P404Component,
+  }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,6 +4,9 @@ import { HomeComponent } from './home/home.component';
 import { VouchersComponent } from './components/vouchers/vouchers.component';
 import { ReciptsComponent } from './components/recipts/recipts.component';
 import { RouterModule } from '@angular/router';
+import { P404Component } from './components/p404/p404.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
 
 
 
@@ -11,7 +14,10 @@ import { RouterModule } from '@angular/router';
   declarations: [
     HomeComponent,
     VouchersComponent,
-    ReciptsComponent
+    ReciptsComponent,
+    P404Component,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     CommonModule,
@@ -19,6 +25,7 @@ import { RouterModule } from '@angular/router';
   ],
   exports: [
     HomeComponent,
+    P404Component,
     // VouchersComponent,
     // ReciptsComponent
   ]
